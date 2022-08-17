6efc28a2a9eb3c1d05839d5abf754dd65d606c07
a.laaribi <a.laaribi@digitiz.ma>
2022-08-17 18:42:36 +0100

    Add Pubilc Module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PublicComponent } from './public/public.component';
+
+const routes: Routes = [
+  {
+    path: 'public',
+    component: PublicComponent,
+    loadChildren: () => import('./public/public.module').then(m=>m.PublicModule)}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
